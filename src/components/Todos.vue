44084e899cef8ae318d8115b0a6f33394f0e3e58
driiich2 <geekdugrenier2@gmail.com>
2019-03-12 18:28:23 +0100

    projet commencé

---
+++ src/components/Todos.vue
@@ -0,0 +1,36 @@
+<template>
+    <section class="todoapp">
+        <header class="header">
+            <h1>Todos</h1>
+            <input type="text" class="new-todo" placeholder="Ajouter une tâche" v-model="newTodo">
+        </header>
+        <div class="main">
+            <ul class="todo-list">
+                <li class="todo" v-for="todo in todos">
+                    <div class="view">
+                        <label>{{ todo.name }}</label>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </section>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                todos: [{
+                    name: 'tâche de test',
+                    completed: false
+                }],
+                newTodo: ''
+
+            }
+        }
+    }
+</script>
+
+<style src="./todos.css">
+
+</style>
