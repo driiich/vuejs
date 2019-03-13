b5b04db440e7c3a9814c2209b87b2e62680793f1
driiich2 <geekdugrenier2@gmail.com>
2019-03-13 13:56:12 +0100

    carousel ok

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <template>
   <div>
+    <carousel>
+      <carousel-slide v-for="n in slides" :index="n - 1">
+        <div style="position:absolute; left:0; right: 0; text-align:center; top: 50%; color: #FFF; font-size:16px;">Slide {{ n }}</div>
+        <img :src="'https://picsum.photos/630/300/?image=' + n" width="100%">
+      </carousel-slide>
+    </carousel>
+    <button @click="addSlide">Ajouter un slide</button>
+    <button @click="removeSlide">Supprimer un slide</button>
     <todos v-model="todos"></todos>
   </div>
 </template>
 
 <script>
     import Todos from './components/Todos'
+    import Carousel from './components/carousel/Carousel'
+    import CarouselSlide from './components/carousel/CarouselSlide'
 
     export default {
         data () {
@@ -32,7 +42,9 @@
             }
         },
         components: {
-            Todos
+            Todos,
+            Carousel,
+            CarouselSlide
         }
     }
 </script>
